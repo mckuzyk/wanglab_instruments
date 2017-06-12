83521f624a7fae1ad31b3856ab233a1d34aff2c3
Mark Kuzyk <mckuzyk@gmail.com>
2017-06-11 20:38:39 -0700

    version 0.1 setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'wanglab_instruments',
+    packages = ['wanglab_instruments'],
+    version = '0.1',
+    description = 'Communication with laboratory equipment',
+    author = 'Mark C Kuzyk',
+    author_email = 'mkuzyk@uoregon.edu',
+    url = 'https://github.com/mckuzyk/wanglab_instruments.git',
+    download_url = 'https://github.com/mckuzyk/wanglab_instruments/archive/0.1..tar.gz',
+    keywords = ['wanglab'],
+    classifiers = [],
+    )
